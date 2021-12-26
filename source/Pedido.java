c1d675c3add443bdf4c5f4c3fdc8af52fb1b0233
ruipedrolousada <ruipedrolousada@gmail.com>
2021-12-26 15:54:17 +0000

    pedido ch

--- source/Pedido.java
+++ source/Pedido.java
@@ -6,37 +6,61 @@ import static java.time.temporal.ChronoUnit.DAYS;
 
 public class Pedido {
 
+    private Integer id;
     private long NIF;
     private LocalDate inicio;
     private LocalDate fim;
     private Integer orcamento;
 
-    private Plano p;
+    private Plano pl;
 
     public Pedido(){
+        this.id = 1;
         this.NIF = 0;
         this.inicio = LocalDate.now();
         this.fim = LocalDate.now();
         this.orcamento = 0;
+        this.pl = new Plano();
+
     }
 
-    public Pedido(long NIF, LocalDate inicio, LocalDate fim, Integer orcamento){
+    public Pedido(Integer id, NIF, LocalDate inicio, LocalDate fim, Integer orcamento, Plano pl){
+        this.id = id;
         this.NIF = NIF;
         this.inicio = inicio;
         this.fim = fim;
         this.orcamento = orcamento;
+        this.pl = pl;
 
     }
 
     public Pedido(Pedido p){
+        this.id = p.getId();
         this.NIF = p.getNIF();
         this.inicio = p.getInicio();
         this.fim = p.getFim();
         this.orcamento = p.getOrcamento();
+        this.pl = p.getPl();
     }
 
     public long daysBetween(LocalDate inicio, LocalDate fim){
         return DAYS.between(inicio, fim);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Plano getPl() {
+        return pl;
+    }
+
+    public void setPl(Plano pl) {
+        this.pl = pl;
     }
 
     public Integer getOrcamento() {
--- source/Plano.java
+++ source/Plano.java
@@ -5,6 +5,11 @@ public class Plano {
     private double totalHoras;
     private double custo;
 
+    public Plano(){
+        this.custo = 0;
+        this.totalHoras = 0;
+    }
+
     public Plano(double totalHoras, double custo) {
         this.totalHoras = totalHoras;
         this.custo = custo;
