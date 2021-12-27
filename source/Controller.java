8cff4374af5fcb82e7be92cc4a4eccb6c6837926
andrepinto42 <andrepinto.goncalves@gmail.com>
2021-12-27 13:24:31 +0000

    Controller

---
+++ source/Controller.java
@@ -0,0 +1,26 @@
+package bin;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import bin.Pedido.Pedido;
+import bin.Pessoas.Pessoa;
+import bin.Pessoas.ReadLoadPessoas;
+
+public class Controller {
+    public  static List<Pessoa> allPessoas = new ArrayList<Pessoa>();
+    public  static Map<LocalDate, Pedido> allPedidos = new HashMap<LocalDate,Pedido>();
+
+    public Controller()
+    {
+        LoadDataBase();
+    }
+    public static void LoadDataBase()
+    {
+        allPessoas = ReadLoadPessoas.ReadAllPessoa();
+
+    }
+}
